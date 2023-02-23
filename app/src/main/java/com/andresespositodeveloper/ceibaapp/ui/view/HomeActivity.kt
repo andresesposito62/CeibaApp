ad16010ff57c4f9ca00a3a9417b13d33effc7735
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2023-02-22 20:23:01 -0500

    feat(app): HomeFragment is add

--- app/src/main/java/com/andresespositodeveloper/ceibaapp/ui/view/HomeActivity.kt
+++ app/src/main/java/com/andresespositodeveloper/ceibaapp/ui/view/HomeActivity.kt
@@ -16,27 +16,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class HomeActivity : AppCompatActivity() {
 
-    var viewModel: HomeViewModelImpl? = null
-    var getUserUseCase: GetUsersUseCase? = null
-    var userRepository: UserRepository? = null
-    var api: Api? = null
-    lateinit var servicesRestApi: ServicesRestApi
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val  retrofit: Retrofit
-                = Retrofit.Builder()
-            .baseUrl(ServicesEndPoints.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
 
-        val servicesRestApi: ServicesRestApi =retrofit.create(ServicesRestApi::class.java)
-
-        api = Api(servicesRestApi)
-        userRepository = UserRepositoryImpl(api!!)
-        getUserUseCase = GetUsersUseCaseImpl(userRepository as UserRepositoryImpl)
-        viewModel = HomeViewModelImpl(getUserUseCase as GetUsersUseCaseImpl)
-        viewModel?.getUsers()
     }
 }
